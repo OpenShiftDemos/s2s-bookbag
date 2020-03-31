593ba873bd4e30927d925da9fe906bf13d008168
Sophie Watson <sophwats@gmail.com>
2020-03-31 15:30:36 -0500

    disabled terminal windows

--- envvars/gateway.sh
+++ envvars/gateway.sh
@@ -14,7 +14,7 @@
 
 # Enable the following line to disable the right-hand pane entirely.
 # Only use this if you have no terminal, no slides, and no console.
-#export DEFAULT_PAGE='workshop'
+export DEFAULT_PAGE='workshop'
 
 # Flags to explicitly enable/disable specific components
 #ENABLE_CONSOLE='false'
